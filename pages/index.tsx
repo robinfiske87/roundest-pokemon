93660c30f84d81571fc68adcd4a6a93d4d184498
Robin Reistad Fiske <robin.reistad.fiske@stepsolutions.no>
2022-10-13 10:46:07 +0200

    added basic structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-
-const Home: NextPage = () => {
+export default function Home() {
   return (
-   <div className="">Hello world</div>
-  )
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
+      <div className="text-2xl text-center">Which Pokemon is Rounder</div>
+      <div className="p-2"/>
+      <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
+        <div className="w-16 h-16 bg-red-200" />
+        <div className="p-8">Vs</div>
+        <div className="w-16 h-16 bg-red-200" />
+      </div>
+    </div>
+  );
 }
-
-export default Home
