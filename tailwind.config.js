0e1d185859f7a11a990d43298b8fd8ff73f4d9b4
Robin Reistad Fiske <robin.reistad.fiske@stepsolutions.no>
2022-10-13 11:14:40 +0200

    restructureing and install new packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  purge: ["./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   content: [],
   theme: {
