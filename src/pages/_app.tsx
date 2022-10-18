3ac26c6e1b9648b3aa47449ff68ec34412b04e00
Robin Reistad Fiske <robin.reistad.fiske@stepsolutions.no>
2022-10-18 14:04:47 +0200

    adjusted and tried to convert from trcp 9.15 to 10. Added poke-API

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,55 +1,17 @@
 import 'tailwindcss/tailwind.css'
 import "../styles/globals.css"
-import type { AppProps } from "next/app";
+import type { AppType } from 'next/app';
+import { trpc } from '../utils/trpc';
 
+const MyApp: AppType = ({ Component, pageProps }) => {
 
-function MyApp ({ Component, pageProps }: AppProps) {
-  const description =
-    "We're here to answer the eternal question: What Pokémon is roundest?";
+  const description = "We're here to answer the eternal question: What Pokémon is roundest?";
   const title = "Roundest Pokémon - Public Poll";
+
   return <Component {...pageProps} />;
 };
 
-import { withTRPC } from '@trpc/next';
-import { AppRouter } from '../server/routers/_app';
-
-function getBaseUrl() {
-  if (process.browser) return ""; // Browser should use current path
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
-
-  return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
-}
-
-export default withTRPC<AppRouter>({
-  config({ ctx }) {
-    /**
-     * If you want to use SSR, you need to use the server's full URL
-     * @link https://trpc.io/docs/ssr
-     */
-    const url = `${getBaseUrl()}/api/trpc`;
-
-    return {
-      url,
-      /**
-       * @link https://react-query.tanstack.com/reference/QueryClient
-       */
-      // queryClientConfig: { defaultOptions: { queries: { staleTime: 60 } } },
-    };
-  },
-  /**
-   * @link https://trpc.io/docs/ssr
-   */
-  ssr: true,
-})(MyApp);
+export default trpc.withTRPC(MyApp);
 
 
 
-
-// import type { AppType } from 'next/app';
-// import { trpc } from '../utils/trpc';
-
-// const MyApp: AppType = ({ Component, pageProps }) => {
-//   return <Component {...pageProps} />;
-// };
-
-// export default trpc.withTRPC(MyApp);
