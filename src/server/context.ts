5adaaad3161e675836009ecfc4b7128482cc0096
Robin Reistad Fiske <robin.reistad.fiske@stepsolutions.no>
2022-10-18 14:16:40 +0200

    cleaned up redundant code for authed session on open app

---
+++ src/server/context.ts
@@ -0,0 +1,30 @@
+// src/server/router/context.ts
+import type { inferAsyncReturnType } from "@trpc/server";
+import type { CreateNextContextOptions } from "@trpc/server/adapters/next";
+
+type CreateContextOptions = {
+};
+
+/** Use this helper for:
+ * - testing, so we dont have to mock Next.js' req/res
+ * - trpc's `createSSGHelpers` where we don't have req/res
+ **/
+export const createContextInner = async (opts: CreateContextOptions) => {
+  return {
+  };
+};
+
+/**
+ * This is the actual context you'll use in your router
+ * @link https://trpc.io/docs/context
+ **/
+export const createContext = async (opts: CreateNextContextOptions) => {
+  const { req, res } = opts;
+
+  // Get the session from the server using the unstable_getServerSession wrapper function
+
+  return await createContextInner({
+  });
+};
+
+export type Context = inferAsyncReturnType<typeof createContext>;
